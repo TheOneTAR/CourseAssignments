093cd0ec8db7fce9dbe7d6e38cdc37bb8f3dd6c4
Tiffany Caires <tar3820@gmail.com>
2016-02-16 17:28:46 -0800

    Revert "Addded link things in Django"
    This reverts commit 2c5e6fd75636ac55c40e43b777f733994858c5ee.

--- Django/portfolio/angry_dice/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AngryIceConfig(AppConfig):
-    name = 'angry_ice'
--- Django/portfolio/port_main/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortMainConfig(AppConfig):
-    name = 'port_main'
